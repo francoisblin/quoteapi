b65660475d157e023eda28916abe2a7613eae86c
François Blin <frablin@gmail.com>
2020-12-02 18:45:48 +0100

    'add get method, post method in server.js'

--- server.js
+++ server.js
@@ -13,6 +13,30 @@ app.get('/api/quotes/random', (req, res, next) => {
   res.send({ quote: randomQuote })
 })
 
+app.get('/api/quotes', (req, res, next) => {
+  const person = req.query.person
+  if (person) {
+    const returnArr = quotes.filter(el => {
+      return el.person === person
+    })
+    res.send({ quotes: returnArr })
+  } else {
+    res.send({ quotes: quotes })
+  }
+})
+
+app.post('/api/quotes', (req, res, next) => {
+  const quoteQuery = req.query.quote
+  const personQuery = req.query.person
+  if (quoteQuery && personQuery) {
+    const toPush = { quote: quoteQuery, person: personQuery }
+    quotes.push(toPush)
+    res.send({ quote: toPush })
+  } else {
+    res.status(400).send()
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`)
 })
